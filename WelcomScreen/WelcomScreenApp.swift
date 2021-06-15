7f2a416146f2a7e8a651bf0e91c803a8fe4b6bd1
张亚飞 <672233580@qq.com>
2021-06-15 21:17:51 +0800

    Initial Commit

---
+++ WelcomScreen/WelcomScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomScreenApp.swift
+//  WelcomScreen
+//
+//  Created by 张亚飞 on 2021/6/15.
+//
+
+import SwiftUI
+
+@main
+struct WelcomScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
